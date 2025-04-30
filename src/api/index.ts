f0a7a5551a82da9d4509fbdde6dc44694baa7021
FerShibli <fernandoluiz.1991@gmail.com>
2025-04-30 15:33:23 -0300

    feat(index): add custom request and response interfaces for improved type safety
    Co-authored-by: Gspedine <gustavospedine@gmail.com>

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { setRoutes } from '../routes';
 import { sequelize } from '../config/database';
+import { Request } from 'express';
+import { Response } from 'express';
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,12 +16,31 @@ sequelize.authenticate()
     .then(() => {
         console.log('Database connection established successfully.');
     })
-    .catch(err => {
+    .catch(err=> {
         console.error('Unable to connect to the database:', err);
     });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+export interface CustomRequest<T> extends Omit<Request, 'body'> {
+    body: T;
+}
+  
+  export interface CustomResponse extends Response {
+    // Pode adicionar propriedades customizadas se necessário
+  }
+  
+  export interface User {
+    phoneNumber: string;
+    firstName?: string;
+    lastName?: string;
+    birthDate?: string;
+    gender?: string;
+    location?: string;
+    email?: string;
+    password?: string;
+  }
 
 export default app;
