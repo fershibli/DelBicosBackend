dbd80c92ad6d39a6b37041a89eb629500b6e7525
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-05 15:50:51 -0300

    feat(migrations): create client table with schema and foreign key references to users and address

---
+++ migrations/20250605184931-client.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const { DataTypes } = require("sequelize");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("client", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      main_address_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: "address",
+          key: "id",
+        },
+      },
+      cpf: {
+        type: DataTypes.STRING(14),
+        allowNull: false,
+        unique: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("client");
+  },
+};
