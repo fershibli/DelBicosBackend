8486a3250714642b88aa9d432b0251fa3aca1a3d
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-05 14:40:44 -0300

    refactor(models): restructure database connection and model loading in index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,47 +1,60 @@
-import { Sequelize } from 'sequelize';
+"use strict";
 
-const sequelize = new Sequelize('database_name', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql', // or 'postgres', 'sqlite', etc.
+import * as fs from "fs";
+import * as path from "path";
+import { Sequelize, DataTypes } from "sequelize";
+import * as process from "process";
+
+// Interface para o objeto de banco de dados
+interface DB {
+  sequelize: Sequelize;
+  Sequelize: typeof Sequelize;
+  [modelName: string]: any;
+}
+
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require(__dirname + "/../config/config.json")[env];
+const db: DB = {} as DB;
+
+let sequelize: Sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(
+    process.env[config.use_env_variable] as string,
+    config
+  );
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter((file: string) => {
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      (file.slice(-3) === ".js" || file.slice(-3) === ".ts") &&
+      file.indexOf(".test.") === -1
+    );
+  })
+  .forEach((file: string) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const model = require(path.join(__dirname, file))(sequelize, DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-const User = sequelize.define('User', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  username: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-const Post = sequelize.define('Post', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  content: {
-    type: Sequelize.TEXT,
-    allowNull: false,
-  },
-  userId: {
-    type: Sequelize.INTEGER,
-    references: {
-      model: User,
-      key: 'id',
-    },
-  },
-});
-
-export { sequelize, User, Post };
+export default db;
