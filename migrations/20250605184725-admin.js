9b485d7e1a34f1aebfaf95fe9d0ff51cb5148e91
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-05 15:49:10 -0300

    feat(migrations): create admin table with schema and foreign key reference to users

---
+++ migrations/20250605184725-admin.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const { DataTypes } = require("sequelize");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("admin", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("admin");
+  },
+};
