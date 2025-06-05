5e30d1b3c1b2b27b5a4b3c8ec1ed5c24217413f3
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-05 14:29:24 -0300

    feat(models): implement User model with Sequelize and define user schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,15 +1,82 @@
-import mongoose, { Schema, model } from 'mongoose';
-import { User } from '../api/';
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "../config/database"; // Adjust the import path as necessary
 
-const userSchema: Schema = new Schema<User>({
-  phoneNumber: { type: String, required: true, unique: true },
-  firstName: { type: String },
-  lastName: { type: String },
-  birthDate: { type: String },
-  gender: { type: String },
-  location: { type: String },
-  email: { type: String, unique: true },
-  password: { type: String },
-});
+/*
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone VARCHAR(13) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    active BOOLEAN DEFAULT TRUE,
+    INDEX active_index_users (active)
+);
+*/
 
-export default model<User>('User', userSchema);
+export interface IUser {
+  id?: number;
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  active?: boolean;
+}
+
+type UserCreationalAttributes = Optional<IUser, "id" | "active">;
+
+export class UserModel extends Model<IUser, UserCreationalAttributes> {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public phone!: string;
+  public password!: string;
+  public active?: boolean;
+
+  // Timestamps
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+UserModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    phone: {
+      type: DataTypes.STRING(13),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+    indexes: [
+      {
+        name: "active_index_users",
+        fields: ["active"],
+      },
+    ],
+  }
+);
