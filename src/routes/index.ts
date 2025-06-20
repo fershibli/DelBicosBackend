cf3ca6aa56914ee0fa9dab8df5613f813eb0c4ea
douglaswenzel <douglas.wenzel@hotmail.com>
2025-06-19 23:51:47 -0300

    feat(routes): add service routes to the main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { IndexController } from '../controllers';
+import serviceRoutes from './serviceRoutes';
+
 
 const router = Router();
 const indexController = new IndexController();
 
 export function setRoutes(app: Router) {
     app.get('/', indexController.getIndex.bind(indexController));
+    app.use('/services', serviceRoutes)
     // Add more routes here as needed
 }
 
