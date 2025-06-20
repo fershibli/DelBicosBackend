1a70e6bb359d17073dd9dfe6c9509fada88f0ef9
douglaswenzel <douglas.wenzel@hotmail.com>
2025-06-20 20:08:06 -0300

    feat(routes): implement user routes for CRUD operations in userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,18 @@
-import { Router } from 'express';
-import { getUser } from '../controllers/homeController';
+import { Router } from "express";
+import {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController";
 
 const router = Router();
 
-router.get('/user/:phoneNumber', getUser);
+router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
