e8189930cb1afa68bdb2e7121a5fc675d1968a2d
isabelmaito <maitoisabel@gmail.com>
2025-06-20 19:13:36 -0300

    refactor(user.routes): import logInUser in user routes for login functionality

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { confirmNumber } from "../controllers/confirmNumberController";
 import { verifyCode } from "../controllers/confirmCodeController";
-import { signUpUser } from "../controllers/user.controller";
+import { logInUser, signUpUser } from "../controllers/user.controller";
 
 const router = Router();
 
 router.post("/register", signUpUser);
+router.post("/login", logInUser);
 router.post("/confirm-number", confirmNumber);
 router.post("/verify-code", verifyCode);
 
