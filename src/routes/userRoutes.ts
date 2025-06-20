400a60768ab04d38df59f55e3184ed601b4d493d
isabelmaito <maitoisabel@gmail.com>
2025-06-20 18:50:15 -0300

    refactor(userRoutes): move user registration and verification routes from authRotes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
-import { getUser } from '../controllers/homeController';
+import { Router } from "express";
+import { confirmNumber } from "../controllers/confirmNumberController";
+import { verifyCode } from "../controllers/confirmCodeController";
+import { signUpUser } from "../controllers/user.controller";
 
 const router = Router();
 
-router.get('/user/:phoneNumber', getUser);
+router.post("/register", signUpUser);
+router.post("/confirm-number", confirmNumber);
+router.post("/verify-code", verifyCode);
 
 export default router;
