b1364fee8950196ebf4341a42ec72d023403db06
isabelmaito <maitoisabel@gmail.com>
2025-06-20 18:36:31 -0300

    feat(routes): reorganize user registration route and import user controller

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,14 @@
-import { Router } from 'express';
-import { confirmNumber } from '../controllers/confirmNumberController';
-import { verifyCode } from '../controllers/confirmCodeController';
-import { register } from '../controllers/registerController';
+import { Router } from "express";
+import { confirmNumber } from "../controllers/confirmNumberController";
+import { verifyCode } from "../controllers/confirmCodeController";
+import { register } from "../controllers/registerController";
+import { signUpUser } from "../controllers/user.controller";
 
 const router = Router();
 
-router.post('/confirm-number', confirmNumber);
-router.post('/verify-code', verifyCode);
-router.post('/register', register);
+router.post("/user/register", signUpUser);
+router.post("/confirm-number", confirmNumber);
+router.post("/verify-code", verifyCode);
+router.post("/register", register);
 
 export default router;
