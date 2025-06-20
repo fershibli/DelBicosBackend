a64468ac490006d44b722699feaf16c18a0b9b3d
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-19 22:57:40 -0300

    fix(server): temporally comment MongoDB section and update default port to 3000

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
-import express, { Express } from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from './src/routes/authRoutes';
-import userRoutes from './src/routes/userRoutes';
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./src/routes/authRoutes";
+import userRoutes from "./src/routes/userRoutes";
 
 // Carregar variáveis de ambiente
 dotenv.config();
@@ -14,20 +14,20 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json());
 
-// Conectar ao MongoDB
-mongoose.connect(process.env.MONGO_URI as string, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-} as any)
-  .then(() => console.log('Conectado ao MongoDB'))
-  .catch((err: any) => console.error('Erro ao conectar ao MongoDB:', err));
+// // Conectar ao MongoDB
+// mongoose.connect(process.env.MONGO_URI as string, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// } as any)
+//   .then(() => console.log('Conectado ao MongoDB'))
+//   .catch((err: any) => console.error('Erro ao conectar ao MongoDB:', err));
 
 // Rotas
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 // Iniciar o servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
