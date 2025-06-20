9c542dd5d3709127c4ceee362e86e231cd0b6a4a
isabelmaito <maitoisabel@gmail.com>
2025-06-20 18:37:07 -0300

    feat(routes): remove unused registration route and controller import

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { confirmNumber } from "../controllers/confirmNumberController";
 import { verifyCode } from "../controllers/confirmCodeController";
-import { register } from "../controllers/registerController";
 import { signUpUser } from "../controllers/user.controller";
 
 const router = Router();
@@ -9,6 +8,5 @@ const router = Router();
 router.post("/user/register", signUpUser);
 router.post("/confirm-number", confirmNumber);
 router.post("/verify-code", verifyCode);
-router.post("/register", register);
 
 export default router;
