a11da206efd54bed3f5f8b41571b46133e95ec00
FerShibli <fernandoluiz.1991@gmail.com>
2025-06-21 21:04:57 -0300

    feat(ClientSeeder): add initial client data seeder for users

---
+++ seeders/20250621233810-initial-client.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // get user ids
+    const users = await queryInterface.sequelize.query(
+      `SELECT id FROM users WHERE name IN ('Fernando', 'Isabel', 'Douglas', 'Gustavo')`,
+      { type: Sequelize.QueryTypes.SELECT }
+    );
+    const userIds = users.map((user) => user.id);
+    // get address ids
+    const addresses = await queryInterface.sequelize.query(
+      `SELECT id FROM address WHERE user_id IN (${userIds.join(",")})`,
+      { type: Sequelize.QueryTypes.SELECT }
+    );
+    const addressIds = addresses.map((address) => address.id);
+    const cpfs = [
+      "53898278999",
+      "18869131459",
+      "39077794018",
+      "57509247772",
+      "08069989971",
+    ];
+    const clients = userIds.map((userId, index) => ({
+      user_id: userId,
+      main_address_id: addressIds[index],
+      cpf: cpfs[index],
+    }));
+    await queryInterface.bulkInsert("client", clients);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("clients", null, {});
+  },
+};
