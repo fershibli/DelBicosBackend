a4e191e567511b24f9476aa0d77e5abcedc98c8c
douglaswenzel <douglas.wenzel@hotmail.com>
2025-06-23 20:08:34 -0300

    feat(professional.model): add relationships for AmenitiesModel and GalleryModel

--- src/models/ProfessionalDTO.ts
+++ src/models/ProfessionalDTO.ts
@@ -1,8 +1,18 @@
-import { DataTypes, Model, Optional, Association, HasManyGetAssociationsMixin, BelongsToGetAssociationMixin } from "sequelize";
+import {
+  DataTypes,
+  Model,
+  Optional,
+  Association,
+  HasManyGetAssociationsMixin,
+  BelongsToGetAssociationMixin,
+  BelongsToManyGetAssociationsMixin,
+} from "sequelize";
 import { sequelize } from "../config/database";
 import { UserModel } from "./User";
 import { AddressModel } from "./Address";
 import { ServiceModel } from "./Service";
+import { AmenitiesModel } from "./Amenities";
+import { GalleryModel } from "./Gallery";
 
 export interface ProfessionalAttributes {
   id: number;
@@ -14,9 +24,13 @@ export interface ProfessionalAttributes {
   updatedAt: Date;
 }
 
-export interface ProfessionalCreationAttributes extends Optional<ProfessionalAttributes, "id" | "main_address_id" | "cnpj"> {}
+export interface ProfessionalCreationAttributes
+  extends Optional<ProfessionalAttributes, "id" | "main_address_id" | "cnpj"> {}
 
-export class ProfessionalModel extends Model<ProfessionalAttributes, ProfessionalCreationAttributes> implements ProfessionalAttributes {
+export class ProfessionalModel extends Model<
+  ProfessionalAttributes,
+  ProfessionalCreationAttributes
+> implements ProfessionalAttributes {
   public id!: number;
   public user_id!: number;
   public main_address_id!: number;
@@ -25,53 +39,57 @@ export class ProfessionalModel extends Model<ProfessionalAttributes, Professiona
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
 
-
   public User?: UserModel;
   public Address?: AddressModel;
   public Services?: ServiceModel[];
+  public Amenities?: AmenitiesModel[];
+  public Gallery?: GalleryModel[];
 
   public getUser!: BelongsToGetAssociationMixin<UserModel>;
   public getAddress!: BelongsToGetAssociationMixin<AddressModel>;
   public getServices!: HasManyGetAssociationsMixin<ServiceModel>;
+  public getAmenities!: BelongsToManyGetAssociationsMixin<AmenitiesModel>;
+  public getGallery!: HasManyGetAssociationsMixin<GalleryModel>;
 
   public static associations: {
     User: Association<ProfessionalModel, UserModel>;
     Address: Association<ProfessionalModel, AddressModel>;
     Services: Association<ProfessionalModel, ServiceModel>;
+    Amenities: Association<ProfessionalModel, AmenitiesModel>;
+    Gallery: Association<ProfessionalModel, GalleryModel>;
   };
 }
 
 ProfessionalModel.init(
   {
-      id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-      },
-      user_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: { model: "users", key: "id" },
-      },
-      main_address_id: {
-          type: DataTypes.INTEGER,
-          allowNull: true,
-          references: { model: "addresses", key: "id" },
-      },
-      cpf: {
-          type: DataTypes.STRING(14),
-          allowNull: false,
-          unique: true,
-      },
-      cnpj: {
-          type: DataTypes.STRING(18),
-          allowNull: true,
-          unique: true,
-      },
-      createdAt: "",
-      updatedAt: ""
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    },
+    main_address_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: { model: "addresses", key: "id" },
+    },
+    cpf: {
+      type: DataTypes.STRING(14),
+      allowNull: false,
+      unique: true,
+    },
+    cnpj: {
+      type: DataTypes.STRING(18),
+      allowNull: true,
+      unique: true,
+    },
+    createdAt: "",
+    updatedAt: "",
   },
-  
   {
     sequelize,
     tableName: "professional",
@@ -79,6 +97,30 @@ ProfessionalModel.init(
   }
 );
 
-ProfessionalModel.belongsTo(UserModel, { foreignKey: "user_id", as: "User" });
-ProfessionalModel.belongsTo(AddressModel, { foreignKey: "main_address_id", as: "address" });
-ProfessionalModel.hasMany(ServiceModel, { foreignKey: "professional_id", as: "services" });
+ProfessionalModel.belongsTo(UserModel, {
+  foreignKey: "user_id",
+  as: "User",
+});
+
+ProfessionalModel.belongsTo(AddressModel, {
+  foreignKey: "main_address_id",
+  as: "address",
+});
+
+ProfessionalModel.hasMany(ServiceModel, {
+  foreignKey: "professional_id",
+  as: "services",
+});
+
+
+ProfessionalModel.hasMany(GalleryModel, {
+  foreignKey: "professional_id",
+  as: "gallery",
+});
+
+ProfessionalModel.belongsToMany(AmenitiesModel, {
+  through: "professional_amenities",
+  as: "amenities",
+  foreignKey: "professional_id",
+  otherKey: "amenity_id",
+});
