ff7e108758caf78ceaea1bffb05b815096f95661
douglaswenzel <douglas.wenzel@hotmail.com>
2025-09-17 14:01:58 -0300

    fix(Users): Create table string fix

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,7 +9,7 @@ CREATE TABLE users (
     phone VARCHAR(13) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     active BOOLEAN DEFAULT TRUE,
-    avatarURI VARCHAR(255),
+    avatarUri VARCHAR(255),
     INDEX active_index_users (active)
 );
 */
