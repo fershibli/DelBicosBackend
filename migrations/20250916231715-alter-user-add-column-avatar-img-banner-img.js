dadc7a1e27799aa03b28d5395d0d78269bdc1186
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-29 01:17:18 -0300

    fix(migrations): rename avatar_img and banner_img columns to avatar_uri and banner_uri in user migration
    Co-Authored-By: IagoYuriRossan <iagorossan321@gmail.com>
    Co-Authored-By: isabelmaito <maitoisabel@gmail.com>
    Co-Authored-By: konsanii <lucas_consani1@hotmail.com>
    Co-Authored-By: Eduardo Kamo <eduardokamoz@gmail.com>

--- migrations/20250916231715-alter-user-add-column-avatar-img-banner-img.js
+++ migrations/20250916231715-alter-user-add-column-avatar-img-banner-img.js
@@ -5,18 +5,18 @@ const { DataTypes } = require("sequelize");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("users", "avatar_img", {
+    await queryInterface.addColumn("users", "avatar_uri", {
       type: DataTypes.STRING(255),
       allowNull: true,
     });
-    await queryInterface.addColumn("users", "banner_img", {
+    await queryInterface.addColumn("users", "banner_uri", {
       type: DataTypes.STRING(255),
       allowNull: true,
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("users", "avatar_img");
-    await queryInterface.removeColumn("users", "banner_img");
+    await queryInterface.removeColumn("users", "avatar_uri");
+    await queryInterface.removeColumn("users", "banner_uri");
   },
 };
