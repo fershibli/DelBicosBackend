1f8854d7023ff48dd57b96e2d9a51b333ef27fe3
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-28 21:50:17 -0300

    fix(migrations): standardize Sequelize data types in service migration

--- migrations/20250916230736-alter-service-add-professional-id-banner-img-columns.js
+++ migrations/20250916230736-alter-service-add-professional-id-banner-img-columns.js
@@ -1,25 +1,26 @@
-'use strict';
+"use strict";
+
+const { DataTypes } = require("sequelize");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('service', 'professional_id', {
-      type: Sequelize.INTEGER,
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("service", "professional_id", {
+      type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'professional',
-        key: 'id',
+        model: "professional",
+        key: "id",
       },
     });
-    await queryInterface.addColumn('service', 'banner_img', {
-      type: Sequelize.STRING(255),
+    await queryInterface.addColumn("service", "banner_img", {
+      type: DataTypes.STRING(255),
       allowNull: true,
     });
-
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('service', 'professional_id');
-    await queryInterface.removeColumn('service', 'banner_img');
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("service", "professional_id");
+    await queryInterface.removeColumn("service", "banner_img");
+  },
 };
