4aebd401018b66890dbc917a2af97012d52b6cd0
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-28 22:34:55 -0300

    fix(migration): correct typo in description type from SRING to STRING(1500)
    Co-Authored-By: IagoYuriRossan <iagorossan321@gmail.com>
    Co-Authored-By: isabelmaito <maitoisabel@gmail.com>
    Co-Authored-By: konsanii <lucas_consani1@hotmail.com>
    Co-Authored-By: Eduardo Kamo <eduardokamoz@gmail.com>

--- migrations/20250605185415-professional.js
+++ migrations/20250605185415-professional.js
@@ -29,7 +29,7 @@ module.exports = {
         },
       },
       description: {
-        type: DataTypes.SRING(1500),
+        type: DataTypes.STRING(1500),
         allowNull: true,
       },
       cpf: {
