074ea749b512b5a2f29de859ca0acd5f6a516ed2
douglaswenzel <douglas.wenzel@hotmail.com>
2025-10-19 17:51:21 -0300

    fix(notification.routes): Rotas definidas corretamente

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { getNotificationsByUser, markNotificationAsRead, markAllNotificationsAsRead } from "../controllers/notification.controller";
+import { getNotificationsByUser, markNotificationAsRead, markAllNotificationsAsRead, saveExpoPushToken } from "../controllers/notification.controller";
 const router = Router();
-router.get('/:userId', /* authMiddleware, */ getNotificationsByUser);
-
-router.patch('/:id/read', /* authMiddleware, */ markNotificationAsRead);
-
-router.patch('/mark-all-read', /* authMiddleware, */ markAllNotificationsAsRead);
+router.get('/:userId', getNotificationsByUser);
+router.patch('/:notificationId/read/:userId', markNotificationAsRead);
+router.patch('/mark-all-read/:userId', markAllNotificationsAsRead);
+router.post('/notifications/save-token', saveExpoPushToken);
 
 
 export default router;
