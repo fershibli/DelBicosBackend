6e8e55931b925da657ca2df4404a9b5217e626ef
douglaswenzel <douglas.wenzel@hotmail.com>
2025-11-21 22:10:04 -0300

    feat(seed):  seeder para notificações de boas-vindas aos primeiros 40 usuarios

---
+++ seeders/015-welcome-notifications.js
@@ -0,0 +1,30 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const welcomeNotifications = [];
+    const NUM_USERS = 40;
+    
+    for (let i = 1; i <= NUM_USERS; i++) {
+      welcomeNotifications.push({
+        user_id: i,
+        title: "🎉 Bem-vindo(a) à Delbicos!",
+        message: 
+          "Obrigado por se juntar a nós. Estamos felizes em tê-lo(a) a bordo. Explore todos os nossos recursos!",
+        is_read: false,
+        notification_type: "system",
+        related_entity_id: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+
+    await queryInterface.bulkInsert("notifications", welcomeNotifications, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("notifications", {
+        notification_type: "system",
+        title: "🎉 Bem-vindo(a) à Delbicos!"
+    }, {});
+  },
+};
