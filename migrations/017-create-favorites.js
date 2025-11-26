1d11dffad4e82d30ca82129db0078865c27aa96f
lucas consani <lucas_consani1@hotmail.com>
2025-11-26 20:43:11 -0300

    feat: create favorites table and add constraints

---
+++ migrations/017-create-favorites.js
@@ -0,0 +1,57 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("favorites", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      professional_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "professional",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+
+    await queryInterface.addConstraint("favorites", {
+      fields: ["user_id", "professional_id"],
+      type: "unique",
+      name: "unique_user_professional_favorite",
+    });
+
+    await queryInterface.addIndex("favorites", ["user_id"], {
+      name: "idx_user_favorites",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("favorites");
+  },
+};
