8eaf2caf603af0c2dedc6599ac70509168209d11
douglaswenzel <douglas.wenzel@hotmail.com>
2026-05-29 18:19:12 -0300

    fix(s3Config): Ajustado credenciais de aceso ao s3

--- src/config/s3Config.ts
+++ src/config/s3Config.ts
@@ -1,8 +1,30 @@
 import { S3Client } from "@aws-sdk/client-s3";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log(
+  "AWS REGION:",
+  process.env.AWS_REGION
+);
+
+console.log(
+  "S3 BUCKET:",
+  process.env.S3_BUCKET_NAME
+);
+
 export const s3Client = new S3Client({
   region: process.env.AWS_REGION,
+
+  credentials: {
+    accessKeyId:
+      process.env.AWS_ACCESS_KEY_ID || "",
+
+    secretAccessKey:
+      process.env.AWS_SECRET_ACCESS_KEY || "",
+
+    sessionToken:
+      process.env.AWS_SESSION_TOKEN,
+  },
 });
