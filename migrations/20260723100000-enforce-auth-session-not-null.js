017b6a9a52b124f1ff094dc0de474db4495cb3e8
IagoYuriRossan <iagorossan321@gmail.com>
2026-08-04 11:01:39 -0300

    feat: Adiciona migrações para garantir auth_session_id não nulo e índice em agendamentos

---
+++ migrations/20260723100000-enforce-auth-session-not-null.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(
+      "UPDATE bot_chat_session " +
+        "SET auth_session_id = CONCAT('user:', user_id) " +
+        "WHERE auth_session_id IS NULL",
+    );
+
+    await queryInterface.changeColumn(
+      "bot_chat_session",
+      "auth_session_id",
+      {
+        type: Sequelize.STRING(64),
+        allowNull: false,
+      },
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn(
+      "bot_chat_session",
+      "auth_session_id",
+      {
+        type: Sequelize.STRING(64),
+        allowNull: true,
+      },
+    );
+  },
+};
